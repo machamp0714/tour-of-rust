f375fa9dfccd7bb56e5a2a5e4c8d0733f4753af3
machamp0714 <machamp0714@gmail.com>
2024-05-11 13:45:40 +0900

    Learn functions

---
+++ functions/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    another_function(5);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1 // セミコロンを付けると「expected `i32`, found `()`」
+}
