0ab71762b58d660d5679147985e9281fb98ce91f
machamp0714 <machamp0714@gmail.com>
2024-05-19 23:07:03 +0900

    Learn enum

--- borrow/src/main.rs
+++ borrow/src/main.rs
@@ -12,16 +12,18 @@ fn main() {
     // let reference_to_nothing = dangle();
 }
 
-fn calculate_length(s: &String) -> usize { // 関数の引数に参照を取ることを借用という
+fn calculate_length(s: &String) -> usize {
+    // 関数の引数に参照を取ることを借用という
     s.len()
 }
 
-fn change(some_string: &mut String) { // 変数がデフォルト不変であるのと同様、参照も不変である
+fn change(some_string: &mut String) {
+    // 変数がデフォルト不変であるのと同様、参照も不変である
     some_string.push_str(", world");
 }
 
-fn dangle() -> &String {
-    let s = String::from("hello");
+// fn dangle() -> &String {
+//     let s = String::from("hello");
 
-    &s // s の参照を返すが、s はスコープを抜けると解放されるため、この参照は無効になる。つまり危険。
-}
+//     &s // s の参照を返すが、s はスコープを抜けると解放されるため、この参照は無効になる。つまり危険。
+// }
---
+++ matching/src/main.rs
@@ -0,0 +1,51 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let coin = Coin::Quarter(UsState::Alaska);
+    println!("result: {}", value_in_cents(&coin));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("six: {:?}", six);
+    println!("none: {:?}", none);
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
+
+fn value_in_cents(coin: &Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
