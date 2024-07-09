d0c3a01b5200e0d57021e498c11eb042d3d2ac0e
machamp0714 <machamp0714@gmail.com>
2024-07-09 22:58:03 +0900

    Learn generics part1

---
+++ generics/src/main.rs
@@ -0,0 +1,36 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    // let number_list = vec![34, 50, 25, 100, 65];
+    // let result = largest(&number_list);
+    // println!("The largest number is {}", result);
+
+    // let char_list = vec!['y', 'm', 'a', 'q'];
+    // let result = largest(&char_list);
+    // println!("The largest char is {}", result);
+
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x);
+}
+
+// binary operation `>` cannot be applied to type `T`
+// fn largest<T>(list: &[T]) -> T {
+//     let mut largest = list[0];
+
+//     for &item in list.iter() {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
