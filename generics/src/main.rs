980e544c0c93c3825a42901737445469bc30675b
machamp0714 <machamp0714@gmail.com>
2024-07-10 23:34:39 +0900

    Learn generics part2

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,6 @@
+// use core::fmt::Debug;
+// use core::fmt::Display;
+
 struct Point<T> {
     x: T,
     y: T,
@@ -9,18 +12,76 @@ impl<T> Point<T> {
     }
 }
 
-fn main() {
-    // let number_list = vec![34, 50, 25, 100, 65];
-    // let result = largest(&number_list);
-    // println!("The largest number is {}", result);
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
 
-    // let char_list = vec!['y', 'm', 'a', 'q'];
-    // let result = largest(&char_list);
-    // println!("The largest char is {}", result);
+pub struct NewArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 
     let p = Point { x: 5, y: 10 };
     println!("p.x = {}", p.x);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+    notify(&tweet);
 }
+
+// pub fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// トレイト境界
+// +構文で複数のトレイトを指定することもできる
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {}
+
+// where句
+// fn some_function<T, U>(t: &T, u: &U) -> i32
+// where T: Display + Clone,
+//       U: Clone + Debug,
+// {}
 
 // binary operation `>` cannot be applied to type `T`
 // fn largest<T>(list: &[T]) -> T {
@@ -34,3 +95,15 @@ fn main() {
 
 //     largest
 // }
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
