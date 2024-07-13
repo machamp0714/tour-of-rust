c2e621045be6ad69e3457af1662eaf9446e3bd41
machamp0714 <machamp0714@gmail.com>
2024-07-13 14:55:36 +0900

    Learn lifetime part2

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,10 +1,18 @@
 fn main() {
-    let r;
+    let string1 = String::from("abcd");
 
     {
-        let x = 5;
-        r = &x;
-    }
+        let string2 = "xyz";
 
-    println!("r: {}", r);
+        let result = longest(string1.as_str(), string2);
+    }
+    println!("The longest string is {}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
